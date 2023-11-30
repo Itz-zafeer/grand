7ac74ca44d7aed614b884e6db12d49fea9194c70
Itz-zafeer <zafeerqz@gmail.com>
2023-12-01 01:04:50 +0500

    origin

--- assets/javascripts/global.js
+++ assets/javascripts/global.js
@@ -6,3 +6,48 @@ function toggleSidebar() {
     sideBar.classList.toggle("active")
     menu.classList.toggle("active")
 }
+function togglePlan() {
+    const plan = document.querySelector(".plan")
+    const planArrow = document.querySelector(".planArrow")
+    plan.classList.toggle("active")
+    planArrow.classList.toggle("active")
+
+}
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
+
+const faqEl = document.querySelectorAll(".faq_Main");
+
+faqEl.forEach((faq) => {
+    faq.addEventListener("click", () => {
+
+        const selectedFaq = faq;
+        faqEl.forEach((v) => {
+            if (v === selectedFaq) {
+                v.classList.toggle("active")
+
+            } else {
+                v.classList.remove("active")
+
+            }
+        })
+
+
+
+    });
+});
